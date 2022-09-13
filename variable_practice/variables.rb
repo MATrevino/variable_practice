d62175d48f0ad92a832e22faebf48d65c65db6de
Meredith Trevino <meredithtrevino@gmail.com>
2022-09-13 12:39:30 -0600

    add puts

--- variable_practice/variables.rb
+++ variable_practice/variables.rb
@@ -6,3 +6,12 @@ amount_of_loans = 50000.00
 amount_for_vacation = 1250.50
 married = false
 happy = true
+
+south_carolina = 1
+email = true
+
+if number_of_pets > 1
+  puts "I'm happy you have at least one pet"
+  If happy = false
+  puts "I'm sorry"
+end
